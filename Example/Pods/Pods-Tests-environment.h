9787f0bb0425bcf85feda28d4655edab9a6e1f19
Danny Li <liyuanzhe1025@gmail.com>
2014-08-14 16:17:54 +0800

    i am going to break some code

--- Example/Pods/Pods-Tests-environment.h
+++ Example/Pods/Pods-Tests-environment.h
@@ -33,6 +33,6 @@
 // travis-ci-demo
 #define COCOAPODS_POD_AVAILABLE_travis_ci_demo
 #define COCOAPODS_VERSION_MAJOR_travis_ci_demo 0
-#define COCOAPODS_VERSION_MINOR_travis_ci_demo 1
-#define COCOAPODS_VERSION_PATCH_travis_ci_demo 0
+#define COCOAPODS_VERSION_MINOR_travis_ci_demo 0
+#define COCOAPODS_VERSION_PATCH_travis_ci_demo 1
 
--- Example/Pods/Pods-travis-ci-demo-environment.h
+++ Example/Pods/Pods-travis-ci-demo-environment.h
@@ -9,6 +9,6 @@
 // travis-ci-demo
 #define COCOAPODS_POD_AVAILABLE_travis_ci_demo
 #define COCOAPODS_VERSION_MAJOR_travis_ci_demo 0
-#define COCOAPODS_VERSION_MINOR_travis_ci_demo 1
-#define COCOAPODS_VERSION_PATCH_travis_ci_demo 0
+#define COCOAPODS_VERSION_MINOR_travis_ci_demo 0
+#define COCOAPODS_VERSION_PATCH_travis_ci_demo 1
 
