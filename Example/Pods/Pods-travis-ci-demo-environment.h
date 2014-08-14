101d2b74d3ffccd46111358ae0bf07902acf97e6
Simon Li <simon824@gmail.com>
2014-08-14 15:32:32 +0800

    hello world

---
+++ Example/Pods/Pods-travis-ci-demo-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// travis-ci-demo
+#define COCOAPODS_POD_AVAILABLE_travis_ci_demo
+#define COCOAPODS_VERSION_MAJOR_travis_ci_demo 0
+#define COCOAPODS_VERSION_MINOR_travis_ci_demo 1
+#define COCOAPODS_VERSION_PATCH_travis_ci_demo 0
+
